e30994772d59bd35462a77903ef43932d542f6ae
Mzar10 <joharivoladylan@gmail.com>
2025-11-21 10:07:14 +0300

    commit

---
+++ Frontend/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import Products from './components/Products.vue';
+export default{
+  components:{
+    Products
+  },
+}
+</script>
+
+<template>
+  <main :style="{fontSize: postFontSize}">
+    <Products></Products>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
